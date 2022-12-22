0f106dd589569846b93b95aaa403ab53228dcf95
snozaki <snozaki@netprotections.co.jp>
2022-12-22 21:16:49 +0900

    mod: ui改善

--- components/MainCardsList.vue
+++ components/MainCardsList.vue
@@ -3,29 +3,30 @@
     dense
     nav
     min-height="536"
-    rounded
   >
     <v-list-item
       v-for="note in notes"
       :key="note.id"
-      link
     >
       <v-list-item-content>
         <v-card
           class="pa-1"
           outlined
         >
-          <v-list-item-title
-            class="text-subtitle-1 ma-1"
-          >
-            <nuxt-link :to="`/notes/${note.id}`"> {{ note.title }} </nuxt-link>
+          <v-list-item-title>
+            <nuxt-link
+              :to="`/notes/${note.id}`"
+              class="ma-2 text-lg-h4 text-decoration-none"
+            >
+              {{ note.title }}
+            </nuxt-link>
           </v-list-item-title>
           <v-list-item-subtitle>
             <v-chip
               class="ma-1 text-caption"
-              label
+              label 
             >
-              {{ note.label ? note.label : 'なし' }}
+              {{ note.tag }}
             </v-chip>
           </v-list-item-subtitle>
           <v-list-item-subtitle>
@@ -33,7 +34,6 @@
               class="ma-1 text-caption"
               label
             >
-              <v-icon left> mdi-clock-time-eight-outline </v-icon>
               {{ note.updatedAt }}
             </v-chip>
           </v-list-item-subtitle>
--- components/MainTagDialog.vue
+++ components/MainTagDialog.vue
@@ -1,79 +1,107 @@
 <template>
-  <v-dialog
-    v-model="sheet"
-    max-width="500"
-  >
-    <template #activator="{ on, attrs }">
-      <v-btn
-        elevation="2"
-        color="secondary"
-        class="white--text justify-centermx-auto ma-3"
-        v-bind="attrs"
-        v-on="on"
-      >
-        タグ
-        <v-icon right> mdi-plus </v-icon>
-      </v-btn>
-    </template>
-    <v-card>
-      <v-btn
-        class="ma-6"
-        text
-        @click="sheet = !sheet"
-      >
-        <v-icon> mdi-window-close </v-icon>
-      </v-btn>
-      <v-card-text
-        class="text-center"
-      >
-        タグを追加します
-      </v-card-text>
+  <div>
+    <v-dialog
+      v-model="tagDialog"
+      max-width="500"
+    >
+      <template #activator="{ on, attrs }">
+        <v-btn
+          elevation="2"
+          color="secondary"
+          class="white--text justify-centermx-auto ma-3"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon left> mdi-tag-outline </v-icon> タグを登録
+        </v-btn>
+      </template>
+      <v-card>
+        <v-btn
+          class="ma-6"
+          text
+          @click="tagDialog = !tagDialog"
+        >
+          <v-icon> mdi-window-close </v-icon>
+        </v-btn>
+        <v-card-text
+          class="text-center"
+        >
+          タグを登録します
+        </v-card-text>
 
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-form
-              class="text-center"
-            >
-              <v-text-field
-                v-model="newTag"
-                outlined
-                class=""
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-form
+                class="text-center"
               >
-              </v-text-field>
-              <v-btn
-                elevation="2"
-                color="primary"
-                class="white--text justify-center mx-auto ma-5"
-                @click="addTag"
-              >
-                保存する
-                <v-icon right> mdi-pencil </v-icon>
-              </v-btn>
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </v-dialog>
+                <v-text-field
+                  v-model="newTag"
+                  :rules="rules.tag"
+                  outlined
+                  required
+                  dense
+                >
+                </v-text-field>
+                <v-btn
+                  :disabled="!isSubmittable"
+                  elevation="2"
+                  color="primary"
+                  class="white--text justify-center mx-auto ma-5"
+                  @click="addTag"
+                >
+                  保存する
+                  <v-icon right> mdi-pencil </v-icon>
+                </v-btn>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ snackbarText }}
+      <v-btn
+        color="primary"
+        text
+        right
+        @click="snackbar = false"
+      >
+        DONE
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      sheet: false,
+      tagDialog: false,
       newTag: '',
+      snackbar: false,
+      snackbarText: '',
+      rules: {
+        tag: [val => (val || '').length > 0 || 'タグを入力してください。']
+      },
     };
   },
   computed: {
     notes() {
       return this.$store.state.tags.list;
-    }
+    },
+    isSubmittable() {
+      return (this.newTag || this.newTag !== '');
+    },
   },
   methods: {
     addTag(event) {
       this.$store.commit('tags/add', this.newTag);
+      this.tagDialog = false;
+      this.snackbar = true;
+      this.snackbarText = `タグ ${this.newTag} を追加しました`;
     }
   }
 }
