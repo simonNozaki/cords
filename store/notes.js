9ce5f4ea8e3eedd0fd906ce0b9df2fba01dd2c8c
snozaki <snozaki@netprotections.co.jp>
2022-12-22 21:16:25 +0900

    mod: 登録するメモのプロパティをフォーマット

--- store/notes.js
+++ store/notes.js
@@ -6,6 +6,18 @@ export const state = () => (
 
 export const mutations = {
   add(state, note) {
+    note.tag = note.tag ? note.tag : 'なし'
+    note.updatedAt = getDateString(note.updatedAt);
     state.list.push(note);
   }
 };
+
+function getDateString(date) {
+  const y = date.getFullYear();
+  const month = date.getMonth();
+  const d = date.getDate();
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+  return `${y}-${month}-${d} ${h}:${m}:${s}`;
+}
