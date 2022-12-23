a620949dc641de3230782b02f7f16cfa8597f885
snozaki <snozaki@netprotections.co.jp>
2022-12-23 23:18:19 +0900

    削除漏れ

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,12 +25,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    {
-      src: '~/plugins/editor.js',
-      mode: 'client'
-    }
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
