72f0c809f86281b0972b9f20546fde769ad61b71
snozaki <snozaki@netprotections.co.jp>
2023-01-24 21:22:51 +0900

    refactor: メニューの項目をコンポーネント化

---
+++ components/atoms/editors/MenuItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    text
+    small
+    class="bubble-menu-button"
+  >
+    <slot></slot>
+  </v-btn>
+</template>
+
+<style scoped>
+.bubble-menu-button {
+  text-transform: none !important;
+}
+</style>
--- components/atoms/editors/RichEditor.vue
+++ components/atoms/editors/RichEditor.vue
@@ -7,83 +7,59 @@
     >
       <v-list>
         <v-list-item class="ma-0 pa-0">
-          <v-card flat>
-            <!-- TODO: コンポーネント化したい -->
-            <v-btn
-              text
-              small
-              class="bubble-menu-button"
-              :class="{ 'is-active': editor.isActive('bold') }"
-              @click="editor.chain().focus().toggleBold().run()"
-            >
-              <v-icon> mdi-format-bold </v-icon> <strong> Bold </strong>
-            </v-btn>
-          </v-card>
+          <MenuItem
+            :class="{ 'is-active': editor.isActive('bold') }"
+            @click="editor.chain().focus().toggleBold().run()"
+          >
+            <v-icon> mdi-format-bold </v-icon> <strong> Bold </strong>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('italic') }"
             @click="editor.chain().focus().toggleItalic().run()"
           >
             <v-icon> mdi-format-italic </v-icon> <em> Italic </em>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
             @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
           >
             <v-icon> mdi-format-header-1 </v-icon>
             <h1>Heading 1</h1>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
             @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
           >
             <v-icon> mdi-format-header-2 </v-icon>
             <h2>Heading 2</h2>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
             @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
           >
             <v-icon> mdi-format-header-3 </v-icon>
             <h3>Heading 3</h3>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
             @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
           >
             <v-icon> mdi-format-header-4 </v-icon>
             <h4>Heading 4</h4>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('bulletList') }"
             @click="editor.chain().focus().toggleBulletList().run()"
           >
@@ -91,29 +67,41 @@
             <ul>
               <li>Bullet list</li>
             </ul>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             :class="{ 'is-active': editor.isActive('blockquote') }"
             @click="editor.chain().focus().toggleBlockquote().run()"
           >
             <v-icon> mdi-format-quote-open </v-icon>
             <blockquote>Block quote</blockquote>
-          </v-btn>
+          </MenuItem>
         </v-list-item>
-        <v-list-item class="pa-0">
-          <v-btn
-            text
-            small
-            class="bubble-menu-button"
+        <v-list-item>
+          <MenuItem
             @click="editor.chain().focus().setHorizontalRule().run()"
           >
             <v-icon> mdi-minus </v-icon> Divider
-          </v-btn>
+          </MenuItem>
+        </v-list-item>
+        <v-list-item>
+          <MenuItem
+            :class="{ 'is-active': editor.isActive('codeBlock') }"
+            @click="editor.chain().focus().toggleCodeBlock().run()"
+          >
+            <v-icon> mdi-xml </v-icon>
+            <pre> <code> Code Block </code> </pre>
+          </MenuItem>
+        </v-list-item>
+        <v-list-item>
+          <MenuItem
+            :class="{ 'is-active': editor.isActive('underline') }"
+            @click="editor.chain().focus().toggleUnderline().run()"
+          >
+            <v-icon> mdi-format-underline </v-icon>
+            <p style="-decoration: underline;"> underline </p>
+          </MenuItem>
         </v-list-item>
       </v-list>
     </BubbleMenu>
@@ -124,11 +112,13 @@
 <script>
 import { Editor, EditorContent, BubbleMenu } from '@tiptap/vue-2'
 import { StarterKit } from '@tiptap/starter-kit'
+import MenuItem from '@/components/atoms/editors/MenuItem'
 
 export default {
   components: {
     EditorContent,
     BubbleMenu,
+    MenuItem,
   },
   props: {
     value: {
@@ -159,6 +149,6 @@ export default {
 
 <style scoped>
 .bubble-menu-button {
-  text-transform: none;
+  -transform: none;
 }
 </style>
