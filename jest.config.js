142ca64d8739c482cf6ac17bf1d2d8ad6bb05776
snozaki <snozaki@netprotections.co.jp>
2023-04-19 21:40:31 +0900

    wip テスト環境の整備途中

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,22 @@
 module.exports = {
+  roots: ['<rootDir>/test'],
+  preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['js', 'vue', 'json', 'ts'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '^.+\\.ts$': 'ts-jest',
+    '.*\\.(vue)$': '@vue/vue3-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)']
 }
